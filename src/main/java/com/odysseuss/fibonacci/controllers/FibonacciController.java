fff3bae2b11df5aba36deae497ba3bcd89c6fad4
Michael Ortiz <12751082+Odysseuss@users.noreply.github.com>
2022-07-23 11:49:37 -0400

    Update FibonacciController.java
    Refactored method to return a long such that more numbers in the sequence can be displayed
    
    Fixed the request mapping

--- src/main/java/com/odysseuss/fibonacci/controllers/FibonacciController.java
+++ src/main/java/com/odysseuss/fibonacci/controllers/FibonacciController.java
@@ -4,17 +4,19 @@ import java.util.Arrays;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/fibonacci")
+@RestController
+@RequestMapping("/fibonacci")
 public class FibonacciController {
 
-    @GetMapping("/fibonacci/{starting}/{numberOfFibonacci}")
-    public Iterable<Integer> fibonacci(@PathVariable(name = "starting")int starting, @PathVariable(name = "numberOfFibonacci") int numberOfFibonacci) {
+    @GetMapping("/{starting}/{numberOfFibonacci}")
+    public Iterable<Long> fibonacci(@PathVariable(name = "starting")int starting, @PathVariable(name = "numberOfFibonacci") int numberOfFibonacci) {
 
-        int[] result = new int[numberOfFibonacci];
-        int onePrevious = 1;
-        int twoPrevious = 0;
+        long[] result = new long[numberOfFibonacci];
+        long onePrevious = 1;
+        long twoPrevious = 0;
 
         boolean started = false;
         for (int i = 0; (i < numberOfFibonacci) ;) {
@@ -31,7 +33,7 @@ public class FibonacciController {
                 i++;
             }
 
-            int temp = onePrevious;
+            long temp = onePrevious;
             onePrevious += twoPrevious;
             twoPrevious = temp;
         }
